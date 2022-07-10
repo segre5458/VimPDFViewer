ed859186d2f3f51849226d53817695547df949bf
segre <segrehouck5458@gmail.com>
2022-07-11 06:55:08 +0900

    複数ページを表示

--- PDFViewer/MainWindow.xaml.cs
+++ PDFViewer/MainWindow.xaml.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.IO;
 using Windows.Data.Pdf;
-using System;
-using System.Threading.Tasks;
-using System.Diagnostics;
-using Windows.Storage.Pickers;
 using Windows.Storage;
 
 namespace PDFViewer
@@ -19,6 +21,8 @@ namespace PDFViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
         private string FileName;
         private List<BitmapSource> pdfPages = new List<BitmapSource>();
         private int DisplayPageNumber = 1;
@@ -38,7 +42,7 @@ namespace PDFViewer
 
             string[] filePath = (string[])e.Data.GetData(DataFormats.FileDrop);
             FileName = filePath[0];
-            LoadPdf(FileName); 
+            LoadPdf(FileName);
         }
 
         private void ToggleButton_File_Loaded(object sender, RoutedEventArgs e)
@@ -71,42 +75,101 @@ namespace PDFViewer
 
         private async void LoadPdf(string fileName)
         {
-            var file = await Windows.Storage.StorageFile.GetFileFromPathAsync(fileName);
-            if(file != null)
+            var file = await StorageFile.GetFileFromPathAsync(fileName);
+            if (file != null)
             {
                 pdfDocument = await PdfDocument.LoadFromFileAsync(file);
                 fileName = System.IO.Path.GetFileName(fileName);
-                readPDFtoImage(0);
+                await readPDFtoImage(pdfDocument);
             }
         }
-        private async Task readPDFtoImage(int pageIndex)
+        private async Task readPDFtoImage(PdfDocument pdfDocument)
         {
-            using(PdfPage page = pdfDocument.GetPage((uint)pageIndex))
+            Debug.WriteLine(pdfDocument.PageCount);
+            BitmapImage[] image = new BitmapImage[pdfDocument.PageCount];
+            for (uint pageNum = 0; pageNum < pdfDocument.PageCount; pageNum++)
             {
-                var options = new PdfPageRenderOptions();
-                using (var stream = new Windows.Storage.Streams.InMemoryRandomAccessStream())
+                using (PdfPage page = pdfDocument.GetPage(pageNum))
                 {
-                    await page.RenderToStreamAsync(stream, options);
+                    var options = new PdfPageRenderOptions();
+                    using (var stream = new Windows.Storage.Streams.InMemoryRandomAccessStream())
+                    {
+                        await page.RenderToStreamAsync(stream, options);
 
-                    BitmapImage image = new BitmapImage();
-                    image.BeginInit();
-                    image.CacheOption = BitmapCacheOption.OnLoad;
-                    image.StreamSource = stream.AsStream();//using System.IOがないとエラーになる
-                    image.EndInit();
-                    imgPDF.Source = image;
-                    imgPDF.Width = image.PixelWidth;
-                    imgPDF.Height = image.PixelHeight;
+                        BitmapImage img = new BitmapImage();
+                        img.BeginInit();
+                        img.CacheOption = BitmapCacheOption.OnLoad;
+                        img.StreamSource = stream.AsStream();
+                        img.EndInit();
+                        image[pageNum] = img;
+                    }
                 }
             }
+            BitmapImage bmp = imageCombine(image);
+            imgPDF.Source = bmp;
+            imgPDF.Width = bmp.PixelWidth;
+            imgPDF.Height = bmp.PixelHeight;
         }
 
-
-        private void ShowPage(int p)
+        private BitmapImage imageCombine(BitmapImage[] bmpimg)
         {
-            if (p < 1) return;
-            if (pdfPages.Count == 0) return;
-            if (p > pdfPages.Count) return;
-            imgPDF.Source = pdfPages[p - 1];
+            Bitmap[] bmp = new Bitmap[bmpimg.Length];
+            for (int i = 0; i < bmpimg.Length; i++)
+            {
+                bmp[i] = BitmapImage2Bitmap(bmpimg[i]);
+            }
+            int dstWidth = 0, dstHeight = 0;
+            for (int i = 0; i < bmp.Length; i++)
+            {
+                if (dstWidth < bmp[i].Width)
+                {
+                    dstWidth = bmp[i].Width;
+                }
+                dstHeight += bmp[i].Height;
+            }
+
+            var dst = new Bitmap(dstWidth, dstHeight);
+            var dstRect = new Rectangle();
+            using (var gs = Graphics.FromImage(dst))
+            {
+                for (int i = 0; i < bmp.Length; i++)
+                {
+                    dstRect.Width = bmp[i].Width;
+                    dstRect.Height = bmp[i].Height;
+                    gs.DrawImage(bmp[i], dstRect, 0, 0, bmp[i].Width, bmp[i].Height, GraphicsUnit.Pixel);
+                    dstRect.Y = dstRect.Bottom;
+                }
+            }
+            return Bitmap2BitmapImage(dst);
+        }
+        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            // BitmapImage bitmapImage = new BitmapImage(new Uri("../Images/test.png", UriKind.Relative));
+
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+        private BitmapImage Bitmap2BitmapImage(Bitmap bmp)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bmp.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+            return bitmapImage;
         }
     }
 }
